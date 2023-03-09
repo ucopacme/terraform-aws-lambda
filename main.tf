45123b2531fa1f862e2dea7a94548687e0d9fb10
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2023-03-09 12:22:19 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+
+data "archive_file" "DataSource" {
+    type = "zip"
+    source_dir  = "scripts"
+    output_path = "outputs/index.js"
+  }
+
+
+
+  resource "aws_lambda_function" "this" {
+    count = var.enabled ? 1 : 0
+    filename = data.archive_file.DataSource.output_path
+    source_code_hash = data.archive_file.DataSource.output_base64sha256
+    function_name = var.function_name
+    role          = var.role
+    handler       = var.handler
+    runtime       = var.runtime
+    timeout       = var.timeout
+    description   = var.description
+    tags          = var.tags
+  }
