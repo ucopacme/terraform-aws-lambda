02c62b6e48886bf2cdf0ba64a7b3d36e0ea4f278
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2023-03-09 12:43:18 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@
 
   resource "aws_lambda_function" "this" {
     count = var.enabled ? 1 : 0
-    filename = data.archive_file.DataSource.output_path
-    source_code_hash = data.archive_file.DataSource.output_base64sha256
+    filename = var.filename
+    source_code_hash = var.source_code_hash
     function_name = var.function_name
     role          = var.role
     handler       = var.handler
