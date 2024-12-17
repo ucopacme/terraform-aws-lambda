b066f990d1a2e4f35a4389554678e42800479ca0
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-12-17 08:36:11 -0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -5,3 +5,14 @@ output "function_name" {
   description = "The name of the Lambda function"
   value       = join("", aws_lambda_function.this.*.function_name)
 }
+
+output "lambda_publish" {
+  value = aws_lambda_function.this[0].publish
+  description = "Indicates whether the Lambda function is published"
+}
+
+
+output "lambda_function_arn_version" {
+  value = "${aws_lambda_function.this[0].arn}:${aws_lambda_function.this[0].version}"
+  description = "The ARN of the Lambda function with version"
+}
