61da5720c324b2c9793fa1dfcc22a1ac3be6d2d9
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-12-17 08:36:34 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,21 @@
-
-  resource "aws_lambda_function" "this" {
-    count = var.enabled ? 1 : 0
-    filename = var.filename
-    source_code_hash = var.source_code_hash
-    function_name = var.function_name
-    role          = var.role
-    handler       = var.handler
-    runtime       = var.runtime
-    timeout       = var.timeout
-    description   = var.description
-    tags          = var.tags
-  }
+resource "aws_lambda_function" "this" {
+  count            = var.enabled ? 1 : 0
+  filename         = var.filename
+  source_code_hash = var.source_code_hash
+  function_name    = var.function_name
+  role             = var.role
+  handler          = var.handler
+  runtime          = var.runtime
+  timeout          = var.timeout
+  description      = var.description
+  tags             = var.tags
+  publish          = var.publish
+}
 
 resource "aws_lambda_permission" "allow_events_bridge_to_run_lambda" {
-    statement_id = "AllowExecutionFromCloudWatch"
-    action = "lambda:InvokeFunction"
-    function_name = join("", aws_lambda_function.this.*.function_name)
-    principal = "events.amazonaws.com"
+  count         = var.enabled ? 1 : 0
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this[0].function_name
+  principal     = "events.amazonaws.com"
 }
